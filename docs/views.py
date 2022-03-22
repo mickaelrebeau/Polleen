fc5718565ffdafe31d4c7e06c8def4d262d83b40
mike <rebeau.mickael@gmail.com>
2022-03-22 18:49:22 +0100

    Polleen commit 7.1.1

--- docs/views.py
+++ docs/views.py
@@ -1,18 +1,21 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+
+from agents.mixins import AgentAndLoginRequiredMixin
 from docs.forms import DocsModelForm
 from docs.models import Doc
 
 
 class DocsListView(LoginRequiredMixin, ListView):
     template_name = 'docs/docs_list.html'
+    context_object_name = 'docs'
 
     def get_queryset(self):
         queryset = Doc.objects.all()
         return queryset
 
 
-class DocsCreateView(LoginRequiredMixin, CreateView):
+class DocsCreateView(AgentAndLoginRequiredMixin, CreateView):
     template_name = 'docs/docs_create.html'
     form_class = DocsModelForm
     success_url = '/docs'
@@ -25,7 +28,7 @@ class DocsCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class DocsDetailView(LoginRequiredMixin, DetailView):
+class DocsDetailView(AgentAndLoginRequiredMixin, DetailView):
     template_name = 'docs/docs_detail.html'
     context_object_name = 'docs'
 
@@ -34,7 +37,7 @@ class DocsDetailView(LoginRequiredMixin, DetailView):
         return queryset
 
 
-class DocsUpdateView(LoginRequiredMixin, UpdateView):
+class DocsUpdateView(AgentAndLoginRequiredMixin, UpdateView):
     template_name = 'docs/docs_update.html'
     model = Doc
     form_class = DocsModelForm
@@ -44,7 +47,7 @@ class DocsUpdateView(LoginRequiredMixin, UpdateView):
         return Doc.objects.all()
 
 
-class DocsDeleteView(LoginRequiredMixin, DeleteView):
+class DocsDeleteView(AgentAndLoginRequiredMixin, DeleteView):
     template_name = 'docs/docs_delete.html'
     model = Doc
     success_url = '/docs'
