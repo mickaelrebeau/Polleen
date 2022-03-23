2b3fb006b0741f710785fb3a2781184d32ae0277
mike <rebeau.mickael@gmail.com>
2022-03-23 18:09:49 +0100

    Polleen commit 8.2.2

--- ia/views.py
+++ ia/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from agents.mixins import AgentAndLoginRequiredMixin
+from ia.models import Ia
 
 
 class IaView(AgentAndLoginRequiredMixin, ListView):
-    pass
+    template_name = 'ia/ia.html'
+    context_object_name = 'ia_list'
+    queryset = Ia.objects.all()
