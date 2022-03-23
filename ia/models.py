b9b86a3a1f393b3fc40f81f6d9394d5afc7db932
mike <rebeau.mickael@gmail.com>
2022-03-23 19:09:56 +0100

    Polleen commit 8.2.3

--- ia/models.py
+++ ia/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from adaptor.model import CsvModel
+from adaptor.fields import CharField
+from django.utils.translation import gettext as _
 
 
-class Ia(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class Ia(CsvModel):
+    name = CharField(max_length=100)
+    description = CharField(blank=True, null=True, default=None)
+    location = CharField(max_length=100, blank=True, null=True, default=None)
+    post = CharField(max_length=100, default=None)
+    company = CharField(max_length=100, default=None)
+    email = CharField(max_length=100, blank=True, null=True, default=None)
+    url_profile = CharField(max_length=200)
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        delimiter = ","
