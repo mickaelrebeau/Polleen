eeb677aa060f17989a078131cd27fbd37268bdcc
mike <rebeau.mickael@gmail.com>
2022-04-06 18:58:05 +0200

    Polleen commit 8.2.5

--- ia/views.py
+++ ia/views.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from django.shortcuts import render
 from django.views.generic import ListView
 from agents.mixins import AgentAndLoginRequiredMixin
@@ -7,4 +8,5 @@ from ia.models import Ia
 class IaView(AgentAndLoginRequiredMixin, ListView):
     template_name = 'ia/ia.html'
     context_object_name = 'ia_list'
-    queryset = Ia.objects.all()
+    data = pd.read_csv('ia/data/ia_data.csv')
+    queryset = (Ia.objects.all(), data)
